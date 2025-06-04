d11da2e98435c12146d3a66e6c689b1bfe97977b
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-06-04 18:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let n = prompt('Enter a Number');
+n = parseInt(n);
+
+let str = '';
+for(let i=1;i<=n;i++){
+	if(i %3 === 0 && i%5 === 0){
+		str = str + '\n'+'FizzBuzz';
+	} else if(i %5 ===0 ){
+		str = str + '\n'+'Buzz';
+	} else if(i %3 === 0){
+		str = str + '\n'+'Fizz';
+	} else {
+		str = str + '\n'+i;
+	}
+}
+
+alert(str);
